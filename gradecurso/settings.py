11cded6e924d717ffaa06c46420cb35ac9faafed
Bernard Rodrigues <bernard.rodrigues@uab.ufjf.br>
2022-10-27 17:51:09 -0300

    gitignore

--- gradecurso/settings.py
+++ gradecurso/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import json
 from pathlib import Path
+
+with open('../scripts/config.json') as f:
+    CONFIG = json.load(f)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k5u)-5--hex9(fhd89*dq9#a6vckduaetnb=x!h7@sh@4acy51'
+SECRET_KEY = CONFIG['SECRET']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
