fcce2ceac719e7087f618aa188bc063379c2f7b4
Bernard Rodrigues <bernard.rodrigues@uab.ufjf.br>
2022-11-01 17:49:37 -0300

    Ressignificação dos botões em processo

--- gradeApp/static/js/button_manager.js
+++ gradeApp/static/js/button_manager.js
@@ -14,6 +14,49 @@ function infoArrayUpdate(){
     })
 }
 
+function disciplinaIdUpdate(periodo){
+    children = $('#disciplinaContainer-' + periodo).children()
+    
+    $.each(children, function(index){
+        group = children.eq(index);
+        index++;
+        group.attr('id', 'group' + periodo + '-' + index);
+        group.children().eq(0).attr('name', 'nome' + periodo + '-' + index);
+        group.children().eq(1).attr('name', 'tipo' + periodo + '-' + index);
+        group.children().eq(2).attr('name', 'categoria' + periodo + '-' + index);
+        group.children().eq(3).attr('name', 'carga-horaria' + periodo + '-' + index);
+        group.children().eq(4).replaceWith($('#group1-1').children().last().clone(true));
+        group.children().eq(4).attr('id', 'delete' + periodo + '-' + index);
+    })
+}
+
+function tableUpdate(){
+    periodoContainers = $('#mainContainer').children()
+    
+    $.each(periodoContainers, function(index){
+        current_periodo = periodoContainers.eq(index)
+        header = current_periodo.children().eq(0).children().eq(0)
+        disciplinaContainer = current_periodo.children().eq(0).children().eq(1)
+        footer = current_periodo.children().eq(0).children().eq(2)
+
+        title = header.children().eq(0)
+        disciplinaRemoveBtn = header.children().eq(1)
+        disciplinaAddBtn = footer.children().eq(0)
+        periodoAddBtn = footer.children().eq(1)
+        
+        current_periodo.attr('id', 'periodoContainer-' + String(index + 1))
+        header.attr('id', 'header' + String(index + 1))
+        disciplinaContainer.attr('id', 'disciplinaContainer-' + String(index + 1))
+        footer.attr('id', 'footer' + String(index + 1))
+        title.text(String(index + 1) + 'º Período')
+        disciplinaRemoveBtn.attr('id', 'disciplinaRemove' + '-' + String(index + 1))
+        disciplinaAddBtn.attr('id', 'disciplinaAdd' + '-' + String(index + 1))
+        periodoAddBtn.attr('id', 'periodoAdd' + '-' + String(index + 1))
+
+        disciplinaIdUpdate(index + 1);
+    })
+}
+
 // Armazenamento do array inicial
 $(document).ready(function(){
     infoArrayUpdate();
@@ -21,19 +64,13 @@ $(document).ready(function(){
 
 // Função triggada ao clicar num botão de Adicionar Disciplina
 $('.disciplinaAdd').click(function(){
-    $(this).prev().append(start_disciplina);
+    periodo = $(this).attr('id').split('-')[1];
+    
+    $('#disciplinaContainer-' + periodo).append(start_disciplina);
 
-    periodo = Number($(this).prev().attr('id').split('-')[1]);
-    disciplina = $(this).prev().children().length;
-
-    new_items = $('#disciplinaContainer-' + periodo).children().last();
-    new_items.attr('id', 'group' + periodo + '-' + disciplina);
-    new_items.children().eq(0).attr('name', 'nome' + periodo + '-' + disciplina);
-    new_items.children().eq(1).attr('name', 'tipo' + periodo + '-' + disciplina);
-    new_items.children().eq(2).attr('name', 'categoria' + periodo + '-' + disciplina);
-    new_items.children().eq(3).attr('name', 'carga-horaria' + periodo + '-' + disciplina);
-    new_items.children().eq(4).replaceWith($('#group1-1').children().last().clone(true));
-    new_items.children().eq(4).attr('id', 'delete' + periodo + '-' + disciplina);
+    disciplina = $(this).parent().prev().children().length;
+    
+    disciplinaIdUpdate(periodo);
 
     if($('#disciplinaContainer-' + String(periodo)).children().length == 2){
         $('#delete' + String(periodo) + '-' + String(disciplina - 1)).removeClass('disabled');
@@ -46,37 +83,34 @@ $('.disciplinaAdd').click(function(){
 })
 
 // Função triggada ao clicar num botão de Adicionar Período
-$('#periodoAdd').click(function(){
-    periodo = info_array.length + 1;
+$('.periodoAdd').click(function(){
+    periodo = Number($(this).attr('id').split('-')[1])
     disciplina = 1;
 
-    $('#mainContainer').append(start_periodo);
+    $('#periodoContainer-' + String(periodo)).after(start_periodo);
+    periodo++;
+
+    if(periodo == 2){
+        $('#periodoRemove-1').removeClass('disabled');
+    }
+
+    //Não consegui entender o que está rolando aqui.....
+    //Os botões perdem os poderes apenas quando adiciono o primeiro período
+    console.log('#disciplinaAdd-' + String(periodo))
+    $('#disciplinaAdd-' + String(periodo)).replaceWith($('#disciplinaAdd-1').clone(true));
+    $('#periodoAdd-' + String(periodo)).replaceWith($('#periodoAdd-1').clone(true));
+    $('#periodoRemove-' + String(periodo)).replaceWith($('.periodoRemove').clone(true));
     
-    periodo_container = $('#mainContainer').children().last();
-    periodo_container.attr('id', 'periodoContainer-' + String(periodo));
-    
-    periodo_container.children().children().children().eq(0).text(periodo + 'º Período');
-    periodo_container.children().children().eq(1).attr('id', 'disciplinaContainer-' + periodo);
-    
-    new_items = $('#disciplinaContainer-' + periodo).children().last();
-    new_items.attr('id', 'group' + periodo + '-' + disciplina);
-    new_items.children().eq(0).attr('name', 'nome' + periodo + '-' + disciplina);
-    new_items.children().eq(1).attr('name', 'tipo' + periodo + '-' + disciplina);
-    new_items.children().eq(2).attr('name', 'categoria' + periodo + '-' + disciplina);
-    new_items.children().eq(3).attr('name', 'carga-horaria' + periodo + '-' + disciplina);
-    new_items.children().eq(4).replaceWith($('#group1-1').children().last().clone(true));
-    new_items.children().eq(4).attr('id', 'delete' + periodo + '-' + disciplina);
-    
-    $('.disciplinaAdd').last().replaceWith($('.disciplinaAdd').first().clone(true));
-    $('.periodoRemove').last().replaceWith($('.periodoRemove').first().clone(true));
+    tableUpdate();
     infoArrayUpdate();
 })
 
-// $('.periodoRemove').click(function(){
-//     $(this).parent().parent().parent().remove();
-
-//     periodo = info_array.length
-// })
+$('.periodoRemove').click(function(){
+    $(this).parent().parent().parent().remove();
+    
+    tableUpdate();
+    infoArrayUpdate();
+})
 
 $('.delete').click(function(){
     periodo = $(this).parent().parent().attr('id').split('-')[1];
@@ -84,21 +118,9 @@ $('.delete').click(function(){
     $(this).parent().remove();
     
     disciplina = Number($('#disciplinaContainer-' + String(periodo)).children().length);
+    
+    disciplinaIdUpdate(periodo)
 
-    
-    children = $('#disciplinaContainer-' + periodo).children()
-    
-    $.each(children, function(index){
-        group = children.eq(index);
-        index++;
-        group.attr('id', 'group' + periodo + '-' + index);
-        group.children().eq(0).attr('name', 'nome' + periodo + '-' + index);
-        group.children().eq(1).attr('name', 'tipo' + periodo + '-' + index);
-        group.children().eq(2).attr('name', 'categoria' + periodo + '-' + index);
-        group.children().eq(3).attr('name', 'carga-horaria' + periodo + '-' + index);
-        group.children().eq(4).attr('id', 'delete' + periodo + '-' + index);
-    })
-    
     if(disciplina == 1){
         $('#delete' + periodo + '-1').addClass('disabled')
     }
