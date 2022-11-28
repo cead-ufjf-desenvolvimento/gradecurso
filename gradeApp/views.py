753a54ed3ad750000964b7b97c6854bfe9cafc8a
Bernard Rodrigues <bernard.rodrigues@uab.ufjf.br>
2022-11-28 08:37:25 -0300

    Geração de imagem através do assistente

--- gradeApp/views.py
+++ gradeApp/views.py
@@ -7,8 +7,6 @@ def index(request):
     return render(request, 'index.html')
 
 def assistente(request):
-    if request.method == 'GET':
-        return render(request, 'assistente.html')
     if request.method == 'POST':
         output = dict(request.POST.items())
         keys = list(output.keys())
@@ -19,17 +17,24 @@ def assistente(request):
             'disciplina_list': []
         }
 
-        for i in range(2, len(output), 4):
+        for i in range(2, len(output), 5):
             context['disciplina_list'].append(
                 {
                     'periodo': int(keys[i].replace('nome','').split('-')[0]),
                     'nome': values[i],
                     'tipo': values[i+1],
                     'categoria': values[i+2],
-                    'carga_horaria': values[i+3]        
+                    'carga_horaria': values[i+3],
+                    'pre_requisito': values[i+4]
                 }
             )
         return render(request, 'output.html', context)
+    
+    elif request.GET.get('baixar'):
+        imgkit.from_string(request.GET.get('baixar'), 'outputs/output.jpg', css=str(settings.STATIC_ROOT) + '/css/table.css')
+        return redirect('download')
+    
+    return render(request, 'assistente.html')
 
 def arquivo(request):
     if request.method == "POST":
@@ -48,7 +53,8 @@ def arquivo(request):
                     'nome': grade_dict['Nome'][i],
                     'tipo': grade_dict['Tipo'][i],
                     'categoria': grade_dict['Categoria'][i],
-                    'carga_horaria': grade_dict['Carga-horária'][i]        
+                    'carga_horaria': grade_dict['Carga-horária'][i],
+                    'pre_requisito': grade_dict['Pré-requisito'][i]       
                 }
             )
         return render(request, 'output.html', context)
